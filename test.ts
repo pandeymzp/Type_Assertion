605cf9aa373577887b6ad43e7d0dd33622dfc0db
pandeymzp <64181688+pandeymzp@users.noreply.github.com>
2020-04-23 09:34:03 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,23 @@
+
+
+
+
+
+// Type Assertion
+
+let typeass :any = 46;
+let typats1 = typeof(<number>typeass)
+let typats2 = typeof(typeass as number)
+// console.log(typats2)
+
+
+
+
+
+
+
+
+
+
+
+
